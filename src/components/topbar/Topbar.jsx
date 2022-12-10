8fc258bcfe1fa43af345cc9631a8b5bf35c8d773
Hossein Aflaki <hossein.aflaki70@gmail.com>
2022-12-10 17:03:21 +0330

    create hambergur menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,23 +1,36 @@
 import "./topbar.scss";
-import { MdOutlinePersonOutline } from "react-icons/md";
-import { MdOutlineMail } from "react-icons/md";
+import { MdOutlinePersonOutline, MdOutlineMail } from "react-icons/md";
+
 import { IconContext } from "react-icons";
 export default function Topbar() {
   return (
-    <IconContext.Provider value={{ color: "blue", size: "4rem" }}>
-      <div className="topbar">
-        <div className="wrapper">
+    <div className="topbar">
+      <div className="wrapper">
+        <IconContext.Provider value={{ color: "", size: "" }}>
           <div className="left">
             <a href="#intro" className="icon">
               genious.
-              <MdOutlinePersonOutline />
-              <MdOutlineMail />
             </a>
+            <div className="itemContainer">
+              <MdOutlinePersonOutline className="materialIcon" />
+              <span>+44 924 12 74</span>
+            </div>
+
+            <div className="itemContainer">
+              <MdOutlineMail className="materialIcon" />
+              <span>jesica@gmail.com</span>
+            </div>
           </div>
 
-          <div className="right">this is right</div>
-        </div>
+          <div className="right">
+            <div className="hamberger">
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
+          </div>
+        </IconContext.Provider>
       </div>
-    </IconContext.Provider>
+    </div>
   );
 }
